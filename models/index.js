a115dc5ad06d86d279614a5389141142d2e804cd
Bbaker1989 <86622638+Bbaker1989@users.noreply.github.com>
2021-11-28 01:17:07 -0500

    user votes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 // import all models
 const Post = require('./Post');
 const User = require('./User');
+const Vote = require('./Vote');
 
 // create associations
 User.hasMany(Post, {
@@ -11,4 +12,35 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+//create the post and user connections
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
+
+//connects User to vote directly
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+
+module.exports = { User, Post, Vote };
